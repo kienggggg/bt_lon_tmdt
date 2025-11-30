6be08f6895eb68a58552c8281016c21f9eae7c86
kienggggg <kiendz2k04@gmail.com>
2025-11-30 18:08:44 +0700

    Fix CORS trailing slash

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     // Cho phép Frontend trên Vercel và Localhost gọi vào
     origin: [
       'http://localhost:3000', 
-      'https://bt-lon-tmdt.vercel.app/' // <-- Thay đúng link Vercel của bạn vào đây
+      'https://bt-lon-tmdt.vercel.app' // <-- Thay đúng link Vercel của bạn vào đây
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
